e5ba4df4adcb948bdb40b0755d0c2fe423db7489
Rashtrapal Shegokar <rashtrapal_shegokar@persistent.com>
2022-08-02 18:43:05 +0530

    Code remove database logic

--- src/main/java/com/chubb/crud/example/controller/ProductController.java
+++ src/main/java/com/chubb/crud/example/controller/ProductController.java
@@ -3,18 +3,12 @@ package com.chubb.crud.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
-
 import com.chubb.crud.example.entity.Product;
-import com.chubb.crud.example.service.ProductService;
-
 import java.util.List;
 
 @RestController
 public class ProductController {
 
-    @Autowired
-    private ProductService service;
-    
     @Autowired
 	private KafkaTemplate<String, Object> template;
 
@@ -24,37 +18,14 @@ public class ProductController {
     @PostMapping("/addProduct")
     public Product addProduct(@RequestBody Product product) {
     	template.send(topic, product);
-        return service.saveProduct(product);
+    	return product;
     }
 
     @PostMapping("/addProducts")
     public List<Product> addProducts(@RequestBody List<Product> products) {
-        return service.saveProducts(products);
+    	template.send(topic, products);
+    	return products;
     }
 
-    @GetMapping("/products")
-    public List<Product> findAllProducts() {
-        return service.getProducts();
-    }
-
-    @GetMapping("/productById/{id}")
-    public Product findProductById(@PathVariable int id) {
-    	template.send(topic, service.getProductById(id));
-        return service.getProductById(id);
-    }
-
-    @GetMapping("/product/{name}")
-    public Product findProductByName(@PathVariable String name) {
-        return service.getProductByName(name);
-    }
-
-    @PutMapping("/update")
-    public Product updateProduct(@RequestBody Product product) {
-        return service.updateProduct(product);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public String deleteProduct(@PathVariable int id) {
-        return service.deleteProduct(id);
-    }
+   
 }
